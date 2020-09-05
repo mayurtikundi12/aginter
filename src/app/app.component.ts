4458f26a6b838417cba4dad1fc7fd0bb4ca86c94
mayur tikundi <mayurtikundi12@gmail.com>
2020-09-05 13:18:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import * as Chart from 'chartjs';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'allgograph';
+  inputValue= new FormControl('');;
+  barChartOptions: ChartOptions = {
+    responsive: true,
+    "scales":{"yAxes":[{ticks:{
+      "beginAtZero":true
+    }}]}
+  };
+  barChartLabels: Label[] = ['1 sept','2 sept','3 sept','4 sept','5 sept'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+
+  barChartData: ChartDataSets[] = [
+    { data: [100,200,300,400,500], label: 'Graphs' }
+  ];
+
+
+
+  add(){
+      const input = Number(this.inputValue.value);
+      let oData = this.barChartData[0]["data"];
+      let resultant = [];
+      for (const value of oData) {
+        resultant.push(Number(value)+input)
+      }
+      this.barChartData[0]["data"] = resultant;
+  }
+
+
+
+  subtract(){
+    const input = Number(this.inputValue.value);
+    let oData = this.barChartData[0]["data"];
+    let resultant = [];
+    for (const value of oData) {
+      resultant.push(Number(value)-input)
+    }
+    this.barChartData[0]["data"] = resultant;
+}
+
+
+multiply(){
+  const input = Number(this.inputValue.value);
+  let oData = this.barChartData[0]["data"];
+  let resultant = [];
+  for (const value of oData) {
+    resultant.push(Number(value)*+input)
+  }
+  this.barChartData[0]["data"] = resultant;
+}
+
+
+divide(){
+  const input =  Number(this.inputValue.value);
+  let oData = this.barChartData[0]["data"];
+  let resultant = [];
+  for (const value of oData) {
+    resultant.push(Number(value)/input)
+  }
+  this.barChartData[0]["data"] = resultant;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {ChartsModule} from 'ng2-charts';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
